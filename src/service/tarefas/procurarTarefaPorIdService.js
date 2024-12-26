c22e026e40d653ebc12cd0d6de9b803b8bebd718
davi <daviforever17@gmail>
2024-12-26 20:40:56 -0300

    aobaa.a.a

--- src/service/tarefas/procurarTarefaPorIdService.js
+++ src/service/tarefas/procurarTarefaPorIdService.js
@@ -5,7 +5,7 @@ export default async function procurarTarefaPorIdService(id) {
     let results = await procurarTarefaPorId(id);
 
     if(results == 0){
-        throw new Error("Nenhuma tarefa com encontrada.")
+        throw new Error("Nenhuma tarefa foi encontrada.")
     }
 
     let tarefa = results[0];
